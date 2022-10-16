c95040c92f4f27e6ee8f356720cd0f1450aa3a79
Changi Cho <eunuch74@gmail.com>
2022-10-16 12:52:36 +0900

    :art: Add counting sort part

--- week22/changicho/9248. Suffix Array/answer.cpp
+++ week22/changicho/9248. Suffix Array/answer.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 // use Manber Myers algorithm by STL sort
 // time : O(N * log_2^2(N))
+// space : O(N)
 vector<int> getSuffixArray(string &s) {
   int size = s.size();
   vector<int> suffixArray(size), rank(size + 1);
@@ -22,22 +23,70 @@ vector<int> getSuffixArray(string &s) {
     };
     sort(suffixArray.begin(), suffixArray.end(), compare);
 
-    vector<int> currentRank(size + 1, 0);
-    currentRank[suffixArray[0]] = 1;
+    vector<int> newRank(size + 1, 0);
+    newRank[suffixArray[0]] = 1;
     for (int i = 1; i < size; i++) {
-      currentRank[suffixArray[i]] = currentRank[suffixArray[i - 1]];
+      newRank[suffixArray[i]] = newRank[suffixArray[i - 1]];
 
       if (compare(suffixArray[i - 1], suffixArray[i])) {
-        currentRank[suffixArray[i]]++;
+        newRank[suffixArray[i]]++;
       }
     }
-    rank = currentRank;
+    rank = newRank;
+  }
+  return suffixArray;
+}
+
+// use Manber Myers algorithm by counting sort
+// time : O(N * log_2(N))
+// space : O(N)
+vector<int> getSuffixArrayByCount(string &s) {
+  int size = s.size();
+  int maximum = max(256, size) + 1;
+
+  vector<int> suffixArray(size), rank(size + size);
+  vector<int> count(maximum), idx(size);
+  // initialize
+  for (int i = 0; i < size; ++i) {
+    suffixArray[i] = i, rank[i] = s[i];
+  }
+  for (int d = 1; d < size; d *= 2) {
+    vector<int> newRank(size + size);
+    auto compare = [&](const int &a, const int &b) {
+      return rank[a] < rank[b] ||
+             (rank[a] == rank[b] && rank[a + d] < rank[b + d]);
+    };
+
+    for (int i = 0; i < maximum; ++i) count[i] = 0;
+    for (int i = 0; i < size; ++i) count[rank[i + d]]++;
+    for (int i = 1; i < maximum; ++i) count[i] += count[i - 1];
+    for (int i = size - 1; i >= 0; --i) idx[--count[rank[i + d]]] = i;
+
+    for (int i = 0; i < maximum; ++i) count[i] = 0;
+    for (int i = 0; i < size; ++i) count[rank[i]]++;
+    for (int i = 1; i < maximum; ++i) count[i] += count[i - 1];
+    for (int i = size - 1; i >= 0; --i)
+      suffixArray[--count[rank[idx[i]]]] = idx[i];
+
+    newRank[suffixArray[0]] = 1;
+    for (int i = 1; i < size; i++) {
+      newRank[suffixArray[i]] = newRank[suffixArray[i - 1]];
+
+      if (compare(suffixArray[i - 1], suffixArray[i])) {
+        newRank[suffixArray[i]]++;
+      }
+    }
+    rank = newRank;
+
+    // reduce running time
+    if (rank[suffixArray[size - 1]] == size) break;
   }
   return suffixArray;
 }
 
 // use Kasai algorithm
 // time : O(N)
+// space : O(N)
 vector<int> getLCP(string &s, vector<int> &suffixArray) {
   int size = s.size();
   vector<int> LCPArray(size), idxOfSuffixArray(size);
@@ -60,7 +109,7 @@ vector<int> getLCP(string &s, vector<int> &suffixArray) {
 }
 
 void solution(string &line) {
-  vector<int> suffixArray = getSuffixArray(line);
+  vector<int> suffixArray = getSuffixArrayByCount(line);
   vector<int> lcp = getLCP(line, suffixArray);
 
   for (int &index : suffixArray) {
@@ -83,7 +132,7 @@ int main() {
   cin.tie(NULL);
   cout.tie(NULL);
 
-  // freopen("./input.txt", "r", stdin);
+  freopen("./input.txt", "r", stdin);
 
   string S;
   cin >> S;
