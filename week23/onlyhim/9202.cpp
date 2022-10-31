496b817ff97643f60ce886b464f25d6634421f7c
onlyhim <1579847@gmail.com>
2022-10-31 20:37:40 +0900

    Update 9202.cpp

--- week23/onlyhim/9202.cpp
+++ week23/onlyhim/9202.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
+#include <unordered_set>
 
 using namespace std;
 
@@ -38,6 +38,17 @@ void makeTrie(string& s, int sidx, trieNode* p) {
 	makeTrie(s, sidx + 1, p->v[idx]);
 }
 
+bool isPrefix(trieNode* p, string& s, int sidx) {
+	if (sidx == s.size()) {
+		return 1;
+	}
+	int idx = toNum(s[sidx]);
+	if (p->v[idx]) {
+		return isPrefix(p->v[idx], s, sidx + 1);
+	}
+	else return 0;
+}
+
 bool find(trieNode* p, string& s, int sidx) {
 	if (sidx == s.size()) {
 		return p->isTerminal;
@@ -52,32 +63,37 @@ bool find(trieNode* p, string& s, int sidx) {
 int score, cnt, longs;
 string longss;
 
-void go(vector<vector<char>>& g, set<string>& sets, trieNode* p, string& s, int y ,int x, int idx) {
+void go(vector<vector<char>>& g, unordered_set<string>& sets, trieNode* p, string& s, int y ,int x, int idx) {
 	if (idx >= 8)	return;
 	for (int i = 0; i < 8; i++) {
 		int ny = goy[i] + y;
 		int nx = gox[i] + x;
 		if (judge(ny, nx)) {
+			if (g[ny][nx] == '.')	continue;
 			s += g[ny][nx];
-			if (find(p, s, 0)) {
+			char curChar = g[ny][nx];
+			g[ny][nx] = '.';
+			if (isPrefix(p, s, 0) && find(p, s, 0)) {
 				if (sets.find(s) == sets.end()) {
 					sets.insert(s);
 					score += points[idx];
 					cnt++;
 					if (idx >= longs) {
-						idx = longs;
+						longs = idx;
 						if (longss.size() < s.size())	longss = s;
 						else if (longss.size() == s.size())	longss = min(longss, s);
 					}
 				}
 			}
 			go(g, sets, p, s, ny, nx, idx + 1);
+			g[ny][nx] = curChar;
 			s.pop_back();
 		}
 	}
 }
 
 int main() {
+
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int w,b;
@@ -92,7 +108,7 @@ int main() {
 	vector<vector<char>> g(4, vector<char>(4));
 	for (int i = 0; i < b; i++) {
 		for (int j = 0; j < 4; j++)	for (int k = 0; k < 4; k++)	cin >> g[j][k];
-		set<string> sets;
+		unordered_set<string> sets;
 		score = 0;
 		cnt = 0;
 		longs = 0;
