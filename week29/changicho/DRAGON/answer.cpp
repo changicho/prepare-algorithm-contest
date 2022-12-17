c243815bf828a51f2803e21d6d215a113a2430e4
Changi Cho <eunuch74@gmail.com>
2022-12-17 15:34:06 +0900

    :art: Create template of code

---
+++ week29/changicho/DRAGON/answer.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int solution(int N) {
+  int answer = 0;
+
+  return answer;
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  // // decimal point
+  // cout << fixed;
+  // cout.precision(8);
+
+  freopen("./input.txt", "r", stdin);
+
+  int C;
+  cin >> C;
+
+  for (int testCase = 1; testCase <= C; testCase++) {
+    int N;
+    cin >> N;
+
+    int answer = solution(N);
+    cout << answer;
+    cout << "\n";
+  }
+
+  return 0;
+}
