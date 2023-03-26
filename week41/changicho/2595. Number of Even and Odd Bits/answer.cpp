b00f2e952e2d1c0fbf59a315bbf7bcb702903824
Changi Cho <eunuch74@gmail.com>
2023-03-26 18:32:32 +0900

    :memo: create descriptions

--- week41/changicho/2595. Number of Even and Odd Bits/answer.cpp
+++ week41/changicho/2595. Number of Even and Odd Bits/answer.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 // use convert and count
-// time : O(log_10(N))
-// space : O(log_10(N))
+// time : O(log_2(N))
+// space : O(log_2(N))
 class Solution {
  private:
   string convert(int n) {
@@ -32,6 +32,28 @@ class Solution {
       if (bits[i] == '1') {
         ans[i % 2]++;
       }
+    }
+    return ans;
+  }
+};
+
+// use convert and count (space optimized)
+// time : O(log_2(N))
+// space : O(1)
+class Solution {
+ public:
+  vector<int> evenOddBit(int n) {
+    // even, odd
+    vector<int> ans = {0, 0};
+
+    int idx = 0;
+
+    while (n > 0) {
+      if (n % 2 == 1) {
+        ans[idx % 2]++;
+      }
+      n /= 2;
+      idx++;
     }
     return ans;
   }
