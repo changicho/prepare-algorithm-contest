add7421104e0964ef27a912bd02d257e20efe514
onlyhim <1579847@gmail.com>
2023-04-10 12:58:06 +0900

    Create 2600.cpp

---
+++ week43/onlyhim/2600.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int ans = 0;
+        if (numOnes >= k)   return k;
+        else{
+            ans += numOnes;
+            k -= numOnes;
+            if (numZeros >= k)  return ans;
+            else{
+                k-= numZeros;
+                ans -=k;
+                return ans;
+            }
+        }
+    }
+};
