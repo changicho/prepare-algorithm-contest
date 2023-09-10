91a8eff4981f39a2bd1ba29ef483260b5fe3db57
Changi Cho <eunuch74@gmail.com>
2023-09-10 23:24:47 +0900

    :art: Add solved code

---
+++ week59/changicho/Problem B: Leapfrog: Ch. 2/B.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+long long MOD = 1e9 + 7;
+
+bool leapfrog(string &line) {
+  int size = line.size();
+
+  int countB = count(line.begin(), line.end(), 'B');
+  int countPoint = count(line.begin(), line.end(), '.');
+
+  return (countB >= countPoint || countB >= 2) && countPoint > 0;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  freopen("input.txt", "r", stdin);
+  // freopen("leapfrog_ch_2_input.txt", "r", stdin);
+
+  int T;
+  cin >> T;
+  for (int testCase = 1; testCase <= T; testCase++) {
+    string L;
+    cin >> L;
+
+    auto answer = leapfrog(L);
+    cout << "Case #" << testCase << ": " << (answer ? "Y" : "N") << "\n";
+  }
+
+  return 0;
+}
