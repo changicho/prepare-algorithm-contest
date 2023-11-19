13d8e6ff87e928e52cf91a1d56957dfdf78866d6
Changi Cho <eunuch74@gmail.com>
2023-11-19 22:42:11 +0900

    :memo: Create description

--- week66/changicho/5885/answer.cpp
+++ week66/changicho/5885/answer.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <set>
 #include <string>
 #include <unordered_set>
@@ -7,43 +8,34 @@
 
 using namespace std;
 
-struct Land {
-  int x, h;
-  bool operator<(const Land &b) const { return h < b.h; }
-};
-
 int solution(vector<int> &heights) {
   // 100,000
   int size = heights.size();
 
   vector<bool> underwater(size, false);
 
-  vector<Land> lands;
-  for (int x = 0; x < size; x++) {
-    lands.push_back({x, heights[x]});
-  }
+  map<int, vector<int>> orderedX;
 
-  sort(lands.begin(), lands.end());
+  for (int x = 0; x < size; x++) {
+    orderedX[heights[x]].push_back(x);
+  }
 
   int numIslands = 1;
   int maxIslands = 1;
-  for (int i = 0; i < size; i++) {
-    int x = lands[i].x;
 
-    underwater[x] = true;
-    bool landToLeft = (x > 0 && !underwater[x - 1]);
-    bool landToRight = (x < size - 1 && !underwater[x + 1]);
+  for (auto &[h, xs] : orderedX) {
+    for (int &x : xs) {
+      underwater[x] = true;
+      bool landToLeft = (x > 0 && !underwater[x - 1]);
+      bool landToRight = (x < size - 1 && !underwater[x + 1]);
 
-    if (landToLeft && landToRight) {
-      numIslands++;
-    } else if (!landToLeft && !landToRight) {
-      numIslands--;
+      if (landToLeft && landToRight) {
+        numIslands++;
+      } else if (!landToLeft && !landToRight) {
+        numIslands--;
+      }
     }
-
-    if ((i == size - 1 || lands[i + 1].h > lands[i].h) &&
-        numIslands > maxIslands) {
-      maxIslands = numIslands;
-    }
+    maxIslands = max(maxIslands, numIslands);
   }
 
   return maxIslands;
@@ -54,7 +46,7 @@ int main() {
   cin.tie(NULL);
   cout.tie(NULL);
 
-  freopen("input.txt", "r", stdin);
+  // freopen("input.txt", "r", stdin);
 
   int N;
   cin >> N;
