a7e27c4ac0c62b41bb3fc00d21d72d071ea99f6d
onlyhim <1579847@gmail.com>
2025-01-20 19:59:48 +0900

    Update B.cpp

--- week95/onlyhim/B.cpp
+++ week95/onlyhim/B.cpp
@@ -1,13 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <vector>
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <numeric>
-#include <set>
-#include <cmath>
+#include <vector>
 #include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
 using namespace std;
 
 int main() {
@@ -29,12 +31,33 @@ int main() {
                 adj[j][k] = str[k] - '0';
             }
         }
-        sort(ans.begin(), ans.end(), [&](int a, int b) {
-            if (adj[a][b] == 1) return a < b;
-            else return a > b;
-            });
-        for (int i = 0; i < n; i++) cout << ans[i]+1 << ' ';
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                int a = ans[j], b = ans[j+1];
+                if (adj[a][b] == 0 && a < b) {
+                    swap(ans[j], ans[j + 1]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) cout << ans[i] + 1 << ' ';
         cout << '\n';
     }
     return 0;
 }
+
+
+/*
+    무향그래프 G와 2차원 배열 E가 주어진다. G는 1~n의 정점으로 구성된다. n <= 10^3
+    E[i][j] = 1인경우, 정점 i,j는 연결되어 있다.
+
+    다음의 조건을 만족하는 정수배열 P를 구해라. 
+    - P는 1~n으로 구성된 permutation이다.
+    - i < j이고 P[i] < P[j]를 만족하는 P[i], P[j]는 인접해있다.
+    - 이러한 P는 유일하다.
+
+    1. P = { 1, 2, ... n }의 형태로 정렬한다.
+    2. P[0]부터 순회하면서 다음을 확인한다.
+    - i < j 이고 i,j가 연결되어 있지 않다면 p[i], p[j]를 swap
+
+*/
